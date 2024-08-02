97b0b0c5328dcfe3c36b8e84bf2f8a61fe03a380
Spencer Olson <solson@instructure.com>
2024-08-02 15:41:37 -0500

    add times to 12 hour report

--- config/compiletime.exs
+++ config/compiletime.exs
@@ -4,3 +4,5 @@ config :weather,
   finch_config: [
     connect_options: [transport_opts: [cacertfile: "#{__DIR__}/../priv/cacerts.pem"]]
   ]
+
+config :elixir, :time_zone_database, Tz.TimeZoneDatabase
