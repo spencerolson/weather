68f8022a08fd53be7d518d7b71fb747e410d404d
Spencer Olson <solson@instructure.com>
2024-08-02 13:06:08 -0500

    fix priv cacerts path
    Allows the escript to be run from a directory other than the root of
    the project.

--- config/compiletime.exs
+++ config/compiletime.exs
@@ -1,4 +1,6 @@
 import Config
 
 config :weather,
-  finch_config: [connect_options: [transport_opts: [cacertfile: "priv/cacerts.pem"]]]
+  finch_config: [
+    connect_options: [transport_opts: [cacertfile: "#{__DIR__}/../priv/cacerts.pem"]]
+  ]
