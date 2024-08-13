3419bb5fae71b35dfc8f129767fa9b8bcba242b8
Spencer Olson <solson@instructure.com>
2024-08-13 14:19:33 -0500

    move finch config to mix.exs
    Moves the finch config from config/compiletime.exs to mix.exs. This
    ensures the configuration is available when this library is used as a
    dependency (in which case the config is ignored).

--- config/compiletime.exs
+++ config/compiletime.exs
@@ -1,8 +1,3 @@
 import Config
 
-config :weather,
-  finch_config: [
-    connect_options: [transport_opts: [cacertfile: "#{__DIR__}/../priv/cacerts.pem"]]
-  ]
-
 config :elixir, :time_zone_database, Tz.TimeZoneDatabase
