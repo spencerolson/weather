1f58a7c8c0d3a57d43632b78d8b7de8afe90eabf
Spencer Olson <solson@instructure.com>
2024-08-14 16:35:02 -0500

    upgrade to version 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Weather.MixProject do
   def project do
     [
       app: :weather,
-      version: "0.1.5",
+      version: "0.2.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
