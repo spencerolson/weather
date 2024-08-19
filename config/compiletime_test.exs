1f73bc407225ee6733620599f8cc586ee0fdcf11
Spencer Olson <solson@instructure.com>
2024-08-19 16:06:56 -0500

    simplify config/config.exs

--- config/compiletime_test.exs
+++
@@ -1,6 +0,0 @@
-import Config
-
-config :weather,
-  req_options: [
-    plug: {Req.Test, Weather.API}
-  ]
--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,10 @@
 import Config
 
-import_config("compiletime.exs")
-import_config("compiletime_#{config_env()}.exs")
+config :elixir, :time_zone_database, Tz.TimeZoneDatabase
+
+if config_env() == :test do
+  config :weather,
+    req_options: [
+      plug: {Req.Test, Weather.API}
+    ]
+end
