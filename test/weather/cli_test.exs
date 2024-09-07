1e0899de95dd7d86a764558155b8c6dc9e1e5d5b
Spencer Olson <solson@instructure.com>
2024-09-07 07:16:46 -0500

    improve --zip docs
    Add information about the format of the zip code to the help text.

--- test/weather/cli_test.exs
+++ test/weather/cli_test.exs
@@ -66,14 +66,14 @@ defmodule Weather.CLITest do
                --color-codes: A comma-separated list of up to 10 color codes to use for colorized output. Values must be integers in the range of 0 to 255 inclusive, or '_' to use the default. Defaults to 202,214,226,148,39,51,15,245,88,9. Values correspond with the following categories: arctic, freezing, cold, chilly, cool, mild, warm, hot, very_hot, scorching. (string)
                --every: Sets the hour interval at which data is reported for the hourly report. Defaults to 3. (integer)
                --hours: Sets the number of hours to report on for the hourly report. Defaults to 12. Max is 48. (integer)
-               --label: The name of the location for which weather data is being fetched. If present, the report will include the label in the output. If not provided but a zip code is provided, the label will be set to the name of the location associated with the zip code.. (string)
+               --label: The name of the location for which weather data is being fetched. If present, the report will include the label in the output. If not provided but a zip code is provided, the label will be set to the name of the location associated with the zip code. (string)
                --latitude: The latitude of the location for which to fetch weather data. (float)
                --longitude: The longitude of the location for which to fetch weather data. (float)
                --api-key: The OpenWeatherMap API key. (string)
                --units: The units in which to return the weather data. Options: "metric" (celsius), "celsius", "imperial" (fahrenheit), "fahrenheit", "standard" (kelvin), "kelvin". (string)
                --test: Fake weather data for testing purposes. Options: "clear", "rain", "storm". (string)
                --twelve: Enables 12-hour time format for the hourly report. Defaults to true. When false, 24-hour time format is used. (boolean)
-               --zip: A zip code string to fetch weather data for. This can be used in place of latitude and longitude. (string)
+               --zip: A zip code string to fetch weather data for. This can be used in place of latitude and longitude. For most accurate results format should be zip/post code and ISO 3166 country code divided by comma. See https://openweathermap.org/api/geocoding-api#direct_zip for more information. (string)
 
                * Note that boolean switches take no values. --<switch> sets the value to true and --no-<switch> sets the value to false.
 
